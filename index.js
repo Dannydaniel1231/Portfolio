6bea50b5495b197d2744eaf781b4ab603ad4afd5
Dannydaniel1231 <ndaniel4040@gmail.com>
2022-09-04 12:26:56 +0200

    inital commit on index.html push to Ines

---
+++ index.js
@@ -0,0 +1,12 @@
+const bod =document.querySelector("body");
+console.log(bod)
+let body=document.getElementById("body")
+console.log(body)
+ let cont=document.getElementById("navaba");
+//  console.log(cont)
+let lft=document.getElementsByClassName("left");
+ let rgt=document.getElementsByClassName("right");
+
+body.addEventListener("click",function(e){
+    
+ })
